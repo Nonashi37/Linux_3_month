5af2a62843357250bf88e3c3ba9343493b88e659
Nonashi37 <argeno37@gmail.com>
2026-03-03 19:45:18 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import asyncio
+
+async def main():
+    print(" Jarvis is online and runnign Async Python in a Venv!")
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+
+print("Hello world")
